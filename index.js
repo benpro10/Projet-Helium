3ad94a10a2ea9691ddaf2977f10002134d5b25da
Exaucé Benjamin Kyalondawa <exaucebenjamink@gmail.com>
2024-05-29 16:27:21 +0100

    final project helium with Js

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
-const Circle1=document.querySelector('.mouseoverCercle1')
-const Circle2=document.querySelector('.mouseoverCercle2')
-const Circle3=document.querySelector('.mouseoverCercle3')
+const Circle1 = document.querySelector(".mouseoverCercle1");
+const Circle2 = document.querySelector(".mouseoverCercle2");
+const Circle3 = document.querySelector(".mouseoverCercle3");
+
+window.addEventListener("mousemove", function (e) {
+  Circle1.style.top = e.pageY + "px";
+  Circle1.style.left = e.pageX + "px";
+  Circle2.style.top = e.pageY + "px";
+  Circle2.style.left = e.pageX + "px";
+  Circle3.style.top = e.pageY + "px";
+  Circle3.style.left = e.pageX + "px";
+});
